64fef1652abaa4fc03cc8589d066eb37b1214a53
malak jabr <malakjabr2@gmail.com>
2023-11-15 20:27:15 +0300

    Completed assignment

--- src/components/Links.js
+++ src/components/Links.js
@@ -6,10 +6,10 @@ function Links(props) {
       <div>
         <h3>Links</h3>
         <a href={props.github} target="_blank" rel="noopener noreferrer">
-          GitHub
+          {props.github}
         </a>
         <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
-          LinkedIn
+          {props.linkedin}
         </a>
       </div>
     );
